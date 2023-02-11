c01fc39bcfedc46b8acd9395af3927b027a7f404
Aman Tiwari <110708815+Aman20114@users.noreply.github.com>
2023-02-11 06:22:22 -0800

    Update script.js

--- script.js
+++ script.js
@@ -8,20 +8,44 @@ let arr = [
 
 function PrintStudentswithMap() {
   //Write your code here , just console.log
+  arr.map(a => {
+    if (a.marks > 50) {
+     console.log(a);
+    }
+  });
 }
 
 function PrintStudentsbyForEach() {
   //Write your code here , just console.log
+  arr.forEach(a => {
+    if (a.marks > 50) {
+      console.log(a);
+    }
+  });
 }
 
 function addData() {
   //Write your code here, just console.log
+  let newArr = { id: 4, name: "susan", age: "20", marks: 45 };
+  arr.push(newArr);
+  console.log(arr);
 }
 
 function removeFailedStudent() {
   //Write your code here, just console.log
+  arr = arr.filter(a => a.marks >= 50);
+  console.log(arr);
+
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  let arr1 = [
+    { id: 7, name: "Golani Umesh", age: "20", marks: 89 },
+    { id: 9, name: "Tiwari Aman", age: "20", marks: 85 },
+    { id: 11, name: "Nishant", age: "20", marks: 89 },
+  ];
+  arr = arr.concat(arr1);
+
+console.log(arr);
 }
